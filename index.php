5542b711d31c4856b8d454a53cb60f18502b8944
blackdracula18 <abubakaryusuf951@gmail.com>
2021-11-18 12:02:19 +0800

    add all the core fundamentals files

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/main.js"></script>
+    </head>
+        <body>
+       
+        <?php echo "Hello World!";?>
+        </body>
+
+    
+</html>
